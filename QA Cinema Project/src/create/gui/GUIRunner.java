6221c5d6864fff54de0cda066323f862e4f1715f
Admin <Admin@STUDENT24>
2018-04-23 14:00:58 +0100

    testing123

--- QA Cinema Project/src/create/gui/GUIRunner.java
+++ QA Cinema Project/src/create/gui/GUIRunner.java
@@ -2,7 +2,8 @@ package create.gui;
 
 public class GUIRunner {
 	
-	public static void main(String[] args) { 		
+	public static void main(String[] args) { 
+		// Run the window
 		CinemaBookingWindow.main(args);
 	}
 
